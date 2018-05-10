2267602c316bbda2b39bc7122708c145cea4e97e
dph <devinhowes@gmail.com>
2018-05-09 20:08:42 -0500

    refactor func to prototype

--- ninja_class_1.js
+++ ninja_class_1.js
@@ -3,20 +3,23 @@ function Ninja (name, health = 100, speed = 3, strength = 3) {
     this.health = health;
     var speed = speed;
     var strength = strength;
-    
-    this.sayName = function() {
-        console.log("My ninja name is", this.name)
-    }
 
     this.showStats = function() {
         console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: " + strength);
-    }
-
-    this.drinkSake = function() {
-        this.health += 10;
-        console.log("You drank Sake and increased your health by 10 to:", this.health);
+        return this;
     }
 }
+
+    Ninja.prototype.sayName = function () {
+        console.log("My ninja name is", this.name)
+        return this;
+    }
+
+    Ninja.prototype.drinkSake = function() {
+        this.health += 10;
+        console.log("You drank Sake and increased your health by 10 to:", this.health);
+        return this;
+    }
 
 var ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
