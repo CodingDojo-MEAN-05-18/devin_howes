bc58fe2364fd3d7bfb15a5df72cbcc539ddd5b22
dph <devinhowes@gmail.com>
2018-05-16 12:07:32 -0400

    math module

---
+++ 2_Node/math_module/mathlib.js
@@ -0,0 +1,17 @@
+module.exports = function(){
+    return {
+        add: function(num1, num2) {
+            console.log('Sum is equal to:', num1 + num2);
+        },
+        multiply: function(num1, num2) {
+            console.log('Total is equal to:', num1 * num2);
+        },
+        square: function(num) {
+            console.log(num, 'squared is equal to:', num^2);
+        },
+        random: function(num1, num2) {
+            var random = Math.random();//look up random property
+            console.log('Random number between', num1, 'and', num2, 'is', random);
+        }
+    }
+};
