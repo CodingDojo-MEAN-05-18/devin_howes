29fc261a06166285eff81f7f2e5c4285a7bca9bb
dph <devinhowes@gmail.com>
2018-05-21 22:57:40 -0400

    added cat data

--- 3_Express/cars_and_cats_ejs/server.js
+++ 3_Express/cars_and_cats_ejs/server.js
@@ -1,6 +1,6 @@
 // start express and assign to app
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // tell express to use the static files folder
 app.use(express.static(__dirname + '/static'));
@@ -21,6 +21,42 @@ app.get('/cats', function (request, response) {
 app.get('/form', function (request, response) {
     response.render('new_car');
 });
+
+// app.get('/cat_info/:id', function (request, response) {
+//     var cat_id = request.params.id;
+app.get('/cat_info/1', function (request, response) {
+    var cat_info = {
+        id: 1, // could use cat_id if using dynamic route
+        name: "Cuddles",
+        age: 3,
+        food: "Salmon",
+        hobbies: ['Sleeping', 'Playing', 'Chasing Mice']
+    };
+    response.render('cat_info', {context: cat_info});
+});
+
+app.get('/cat_info/2', function (request, response) {
+    var cat_info = {
+        id: 2,
+        name: "Zippy",
+        age: "6 mos",
+        food: "Tuna",
+        hobbies: ['Playing', 'Bouncing', 'Chasing Mice']
+    };
+    response.render('cat_info', { context: cat_info });
+});
+
+app.get('/cat_info/3', function (request, response) {
+    var cat_info = {
+        id: 3,
+        name: "Max",
+        age: 7,
+        food: "Edible",
+        hobbies: ['Sleeping', 'Napping']
+    };
+    response.render('cat_info', { context: cat_info });
+});
+
 
 // tell express what port to listen on
 app.listen(8000, function() {
