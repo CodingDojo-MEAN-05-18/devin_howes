55e5f6c106de9d5ffa2bec9b78e8bc64e05861a9
dph <devinhowes@gmail.com>
2018-05-23 17:38:56 -0400

    counter

--- 3_Express/counter/server.js
+++ 3_Express/counter/server.js
@@ -1,18 +1,39 @@
 var express = require('express');
 var path = require('path');
+var session = require('express-session');
 var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './static')));
+app.use(session({
+    secret: 'bigsecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-// routes
-app.get('/', function(req, res){ 
+if (!session.count) {
+    session.count = 0;
+};
 
-    res.render('index');
+// routes
+app.get('/', function(req, res){
+    session.count++;
+    res.render('index', {count: session.count});
+});
+
+app.get('/add2', function (req, res) {
+    session.count++;
+    res.redirect('/');
+});
+
+app.get('/reset', function(req, res){
+    session.count = 0;
+    res.redirect('/');
 });
 
 // port
