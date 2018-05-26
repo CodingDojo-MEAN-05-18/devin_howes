067827e1aea5965867072e7d4e884df6de8bb96d
dph <devinhowes@gmail.com>
2018-05-26 10:51:09 -0400

    before routes

--- 3_Express/group_chat/server.js
+++ 3_Express/group_chat/server.js
@@ -29,44 +29,51 @@ const server = app.listen(port, function () {
     console.log(`Listening on port ${ port }`);
 });
 
-// route
-app.get('/', function(req, res){
-    res.render('index', {id: session.user_id});
-});
-
 // socket code
 const io = require('socket.io')(server);
 let all_users = [];
 let chats = [];
-let current_user = session.id;
 
 if (!session.count) {
     session.count = 0;
-    session.user_id = 0;
 };
 
 io.on('connection', function(socket) {
-    console.log('user connected');
-    
+    console.log(`user ${ session.count } connected`);
+
     // listen for new user name:
     socket.on('got_new_user', function(req){
-        all_users.push({id: session.id, name: req.name});
         session.user_id = session.count;
         session.count++;
-        io.emit('new_user', {new_user: req.name});
+
+        session.user_name = req.name;
+
+        all_users.push({
+            id: session.user_id,
+            name: session.user_name
+        });
+
+        if (all_users.length !== 0){
+            io.emit('new_user', {new_user: req.name});
+            io.emit('existing_users', all_users);
+        }
     });
 
     // listen for new messages:
     socket.on('new_message', function(data){
-        // console.log('message' + data);
-        chats.push({user: data.user, message: data.message});
+        chats.push({user_id: data.user, user_name: data.user_name, message: data.message});
         io.emit('update_chat', data);
     });
 
     // every user sees all users and chats:
-    socket.emit('existing_users', all_users);
-    console.log(all_users);
-
     socket.emit('chat_history', chats);
-    console.log(chats);
+    
+});
+
+// route
+app.get('/', function (req, res) {
+    res.render('index', {
+        id: session.user_id,
+        name: session.user_name
+    });
 });
