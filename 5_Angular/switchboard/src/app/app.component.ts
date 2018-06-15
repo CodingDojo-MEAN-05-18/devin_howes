eb7e926c7cb271c19f1d89d1e220e22700c635c6
dph <devinhowes@gmail.com>
2018-06-14 20:19:10 -0400

    switchboard

--- 5_Angular/switchboard/src/app/app.component.ts
+++ 5_Angular/switchboard/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Switchboard';
+  buttons = [true, false, true, false, true, false, true, false, true, false];
+  
+
+  onButtonClick(idx) {
+    this.buttons[idx] = !this.buttons[idx]
+  }
 }
